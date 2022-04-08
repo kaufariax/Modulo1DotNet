04474a2c2bcf4e426e0bee96521f49618ab99a51
kaufariax <eikauanefarias@gmail.com>
2022-04-08 15:53:12 -0300

    @kaufariax - exercicio de classes

---
+++ ExerciciosCS/List8Ex/List8Ex/Program.cs
@@ -0,0 +1,28 @@
+﻿using List8Ex.crs;
+using System;
+
+namespace List8Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cachorros");
+            Cachorro Scooby = new Cachorro("dourado", "golden","Scooby", 27.5 );
+            Cachorro Tobby = new Cachorro("preto", "poodle", "Tobby", 12.7 );
+            Cachorro Luidge = new Cachorro("marrom", "labrador", "Luidge", 25.8 );
+
+            Console.WriteLine($"O Cachorro 1 se chama {Scooby.Nome}, da raça {Scooby.Raca}, da cor {Scooby.Cor} e tem um peso de {Scooby.Peso}Kg");
+            Scooby.comer("Frango");
+            Scooby.latir();
+
+            Console.WriteLine($"O Cachorro 2 se chama {Tobby.Nome}, da raça {Tobby.Raca}, da cor {Tobby.Cor} e tem um peso de {Tobby.Peso}Kg");
+            Tobby.comer("Carne");
+            Tobby.latir();
+
+            Console.WriteLine($"O Cachorro 3 se chama {Luidge.Nome}, da raça {Luidge.Raca}, da cor {Luidge.Cor} e tem um peso de {Luidge.Peso}Kg");
+            Luidge.comer("Carne com Batata");
+            Luidge.latir();
+        }
+    }
+}
---
+++ ExerciciosCS/List8Ex/List8Ex/crs/Cachorro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List8Ex.crs
+{
+    internal class Cachorro
+    {
+
+        public string Cor { get; set; }
+        public string Raca { get; set; }
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+
+        public Cachorro(string _cor, string _raca, string _nome, double _peso)
+        {
+            Cor = _cor;
+            Raca = _raca;
+            Nome = _nome;
+            Peso = _peso;
+        }
+
+        public void comer(string racao)
+        {
+            Console.WriteLine($"a ração que eu estou comendo é de {racao}");
+        }
+        public void latir()
+        {
+            Console.WriteLine("AUAUAUAUAUAUAU");
+        }
+    }
+}
