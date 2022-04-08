35af1d8dc41471b1f1e56e8ca28b735bdf92807c
kaufariax <eikauanefarias@gmail.com>
2022-04-08 14:38:57 -0300

    @kaufariax - aula classes

---
+++ ExerciciosCS/Aula8/Program.cs
@@ -0,0 +1,25 @@
+﻿using Aula8.crs;
+using System;
+
+namespace Aula8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GATOS");
+            Gato garfild = new Gato("Garfilf", "Laranja", 8);
+            Gato tom = new Gato("Thomas", "cinza", 5);
+            Gato frajola = new Gato();
+
+            Console.WriteLine($"gato 1 é o {garfild.Nome} de cor {garfild.Cor} e tamanho {garfild.Tamanho}");
+            Console.WriteLine($"gato 2 é o {tom.Nome} de cor {tom.Cor} e tamanho {tom.Tamanho}");
+            Console.WriteLine($"gato 3 é o {frajola.Nome} de cor {frajola.Cor} e tamanho {frajola.Tamanho}");
+
+            garfild.comer("lasanha");
+            tom.dormir();
+            string retorno = frajola.ronronar();
+            Console.WriteLine(retorno);
+        }
+    }
+}
---
+++ ExerciciosCS/Aula8/crs/Gato.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula8.crs
+{
+    class Gato
+    {
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public int Tamanho { get; set; }
+
+        public Gato()
+        {
+
+        }
+
+        public Gato(string _nome, string _cor, int _tamanho)
+        {
+            Nome = _nome;
+            Cor = _cor;
+            Tamanho = _tamanho;
+        }
+
+        public void dormir()
+        {
+            Console.WriteLine("ZZZZzzzzzzzZZZz");
+        }
+
+        public void comer(string comida)
+        {
+            Console.WriteLine($"estou comendo {comida}");
+        }
+
+        public string ronronar()
+        {
+            return "ronroneando";
+        }
+    }
+}
